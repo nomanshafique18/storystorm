377d9253cff9a5acabc9afcacddf03de56cf7711
nomanshafique18 <41620331+nomanshafique18@users.noreply.github.com>
2021-10-04 19:19:20 +0500

    artlink

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -227,7 +227,7 @@ export default function Profile() {
                   <p class="grayColor text-center fs-15 mb-4">Choose template to get started</p>
                   <div class="row">
                     <div class="col-sm-4  w-30-m">
-                        <Link to="https://storystorm.vercel.app/art#/art" class="text-center storyCont">
+                        <Link to="/art" class="text-center storyCont">
                           <img src="./images/dummy.png" class="rounded img-fluid" />
                           <h5 class="defaultColor mt-4 mb-1">Manga</h5>
                           <p>5 in x 7.5 in</p>
@@ -235,14 +235,14 @@ export default function Profile() {
                     </div>
                     
                     <div class="col-sm-4 w-30-m">
-                        <a href="https://storystorm.vercel.app/art#/art" class="text-center storyCont">
+                        <a href="/art" class="text-center storyCont">
                           <img src="./images/dummy.png" class="rounded img-fluid" />
                           <h5 class="defaultColor mt-4 mb-1">Webtoon</h5>
                           <p>800 in x 1280 in</p>
                         </a>
                     </div>
                     <div class="col-sm-4 w-30-m">
-                        <a href="https://storystorm.vercel.app/art#/art" class="text-center storyCont">
+                        <a href="/art" class="text-center storyCont">
                           <img src="./images/dummy.png" class="rounded img-fluid" />
                           <h5 class="defaultColor mt-4 mb-1">Comic</h5>
                           <p>6.256 in x 10.25 in</p>
